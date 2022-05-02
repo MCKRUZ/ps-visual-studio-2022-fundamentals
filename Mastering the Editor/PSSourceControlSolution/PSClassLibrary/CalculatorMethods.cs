a03554a52c57ac830d33bd7a982d795f7f6af31d
MCKRUZ <kruz79@gmail.com>
2022-05-02 10:27:51 -0400

    Added some logging

--- Mastering the Editor/PSSourceControlSolution/PSClassLibrary/CalculatorMethods.cs
+++ Mastering the Editor/PSSourceControlSolution/PSClassLibrary/CalculatorMethods.cs
@@ -4,21 +4,25 @@
 	{
 		public static float Add(float x, float y)
 		{
+			Console.WriteLine("Adding {0} minus {1}", x, y);
 			return x + y;
 		}
 
 		public static float Subtract(float x, float y)
 		{
+			Console.WriteLine("Subtracting {0} minus {1}", x, y);
 			return x - y;
 		}
 
 		public static float Multiply(float x, float y)
 		{
+			Console.WriteLine("Multiplying {0} minus {1}", x, y);
 			return x * y;
 		}
 
 		public static float Divide(float x, float y)
 		{
+			Console.WriteLine("Dividing {0} minus {1}", x, y);
 			if (y == 0)
 				return 0;
 			else
